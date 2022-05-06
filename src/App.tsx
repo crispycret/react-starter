d0cb168528162104f279092c172db6ab79fb8eac
brandon nadeau <bnadeau@uncc.edu>
2022-05-06 03:15:06 -0400

    Working with hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState, useEffect, useRef } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+
+
+
+
+function Header (props:any) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{ props.text ? 'None' : '' + props.text }</h2>
     </div>
   );
 }
 
+
+
+
+function Home (props:any) {
+  return (
+    <div>
+      <h2>Ham</h2>
+    </div>
+  );
+} 
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Header props={{text:"Login"}} />} />
+          <Route path='/dashboard' element={<Header props={{text:"Dashboard"}} />} />
+        </Routes>
+      </BrowserRouter>
+
+
+    </div>
+  );
+}
+
+
+
+
 export default App;
+
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import './assets/bootstrap/5.1.3/css/bootstrap.min.css'
+
+import './assets/css/index.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+import App from './App';
+
+library.add(fab, faCheckSquare, faCoffee)
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
